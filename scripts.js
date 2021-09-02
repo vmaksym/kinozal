f7dc5f1c7f666ffdafb7082723be74d19e63a558
vmaksym <maksymchuk.v@gmail.com>
2021-09-02 21:11:54 +0300

    fetch data

--- scripts.js
+++ scripts.js
@@ -1,27 +1,29 @@
-function appendContent(content) {
-    const el = document.querySelector(".content");
-    el.appendChild(content);
-  }
-  function createFragmentTemplate(str){
-    var template = document.createElement("template");
-    template.innerHTML = str;
-    return template.content;
-  }
-  function createMovieCard(movie) {
-    const article = `<article class="card">
-      <header>
-        <h2>${movie.name}</h2>      
-      </header>
-      <section class="card__content">
-        <img src="${movie.thumb}" alt="${movie.name}">
-        <section>
-          <p>${movie.date}</p>
-          <hr>
-          <p>${movie.description}</p>
-          <hr>
-          <p>Director: ${movie.director}</p>
-        </section>
+function appendContent(to, content) {
+  const el = document.querySelector(`.${to}`);
+  const fragment = createFragmentTemplate(content);
+  el.appendChild(fragment);
+}
+
+function createFragmentTemplate(str){
+  const template = document.createElement("template");
+  template.innerHTML = str;
+  return template.content;
+}
+
+function createMovieCard(movie) {
+  return `<article class="card">
+    <header>
+      <h2>${movie.name}</h2>      
+    </header>
+    <section class="card__content">
+      <img src="${movie.thumb}" alt="${movie.name}">
+      <section>
+        <p>${movie.date}</p>
+        <hr>
+        <p>${movie.description}</p>
+        <hr>
+        <p>Director: ${movie.director}</p>
       </section>
-    </article>`;
-    return createFragmentTemplate(article);
-  }
+    </section>
+  </article>`;
+}
