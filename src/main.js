cb9d9f8b6148ec4baee74c3c36ff4c832568cf75
vmaksym <maksymchuk.v@gmail.com>
2021-09-03 10:56:16 +0300

    restructure

--- src/main.js
+++ src/main.js
@@ -2,20 +2,28 @@ import { appendContent } from "./scripts.js"
 import { createNavigation } from "./navigation.js"
 import { createMovieCard } from "./card.js"
 
+const sortMovies = data => {
+    const sort = new URLSearchParams(location.search).get('sort');
+    switch (sort) {
+        case 'name':
+            return data.sort((a,b) =>
+                a.name.localeCompare(b.name));
+        case 'date':
+            return data.sort((a,b) =>
+                new Date(a.date).getTime() - new Date(b.date).getTime());
+        default:
+            return data;
+    }
+}
+
 // create menu
 appendContent('navigation', createNavigation());
 
 // get movies list
 fetch('https://run.mocky.io/v3/d233f719-e34b-4c0e-bc21-103a931f4bc4')
-  .then((response) => response.json())
-  .then(data => {
-    const sort = new URLSearchParams(location.search).get('sort');
-    switch (sort) {
-      case 'name': return data.sort((a,b) => a.name.localeCompare(b.name));
-      case 'date': return data.sort((a,b) => new Date(a.date).getTime() - new Date(b.date).getTime());
-      default: return data;
-    }
-  })
+  .then((response) =>
+      response.json())
+  .then(sortMovies)
   .then((data) =>
     data.forEach(movie =>
         appendContent('content', createMovieCard(movie))));
